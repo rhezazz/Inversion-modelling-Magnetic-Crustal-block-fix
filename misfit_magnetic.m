348d327849a23b3f2993781214caf977b23e35b5
Mohammad Rheza Zamani <56064595+rhezazz@users.noreply.github.com>
2022-01-03 23:33:09 +0700

    Delete misfit_magnetic.m

--- misfit_magnetic.m
+++
@@ -1,8 +0,0 @@
-%Objective function for magnetic data inversion
-function [misfit] = misfit_magnetic(dBz_obs, dBz_cal)
-    ls = length(dBz_obs);
-    for j = 1 : ls
-        m(j) = ((dBz_cal(j) - (dBz_obs(j))))^2;
-    end
-    misfit = sqrt((1/ls)*sum(m));
-end
